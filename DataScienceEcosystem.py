49e658920ce163e310ea6722c8d1b2fe0dd43654
amarojba <107173699+amarojba@users.noreply.github.com>
2023-08-03 21:58:50 -0600

    DataScience Ecosystem
    Complete notebook for Coursera Tools for Data Science exercise

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # H1: Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives**:
+# - Popular languages used by Data Scientists
+# - Commonly used libraries by Data Sicentists
+# - Useful open source tools for Data Scientists
+# 
+# 
+
+# Some of the popular languages that Data Scientists use are:
+#     1. Python
+#     2. C++
+#     3. R
+#     
+
+# Some of the commonly used libraries used by Data Scientists include:
+#     1. dplyr
+#     2. stringr
+#     3. ggplot
+#     4. caret
+
+# | Data Science Tools |
+# | -------------------|
+# | RSTUDIO |
+# | ANACONDA |
+# | VISUAL STUDIO CODE|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[2]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+print((3*4)+5)
+
+
+# In[3]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+print(200/60)
+
+
+# ## Author
+# Juan Amaro Alaffa Benavides
+
+# In[ ]:
+
+
+
+
